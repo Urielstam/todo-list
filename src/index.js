3fa0686635ecb91d2cbd2da19f1d586828aee4a0
Urielstam <urielstamelman@gmail.com>
2023-01-01 12:23:23 +0200

    Set up first design structure with theme changing functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
-import './style.css'
+import './style.css';
 
-console.log("WOrking....");
+// TODO Create todoList array object to store all todos
+// TODO Create todo items dynamically with factory functions
+
+const todoList = (() => {
+
+    let _defaultTodoList = [];
+
+    const getDefaultTodoList = () => {
+        console.log(_defaultTodoList)
+        return _defaultTodoList;
+    }
+
+    const addTodo = (todo) => {
+        _defaultTodoList.push(todo);
+    }
+
+    return {
+        getDefaultTodoList: getDefaultTodoList,
+        addTodo: addTodo
+    }
+
+})();
+
+
+const todoFactory = (title, desc, priority, dueDate, dateCreated) => {
+
+    const deleteTodo = () => {
+        // Delete
+    }
+
+    const editTodo = () => {
+        // Edit
+    }
+
+    const viewDetails = () => {
+        // View Details
+    }
+
+    const addSubTask = () => {
+        // Add sub tasks
+    }
+
+    return {
+        title,
+        desc,
+        priority,
+        dueDate, 
+        dateCreated, 
+        deleteTodo, 
+        editTodo, 
+        viewDetails, 
+        addSubTask
+    };
+}
+
+const btn = document.querySelector('.open-aside');
+btn.addEventListener('click', (e) => {
+    const side = document.querySelector('.sidebar');
+    side.classList.toggle('is-visible')
+
+})
+
+const toggleTheme = document.getElementById('theme-mode');
+toggleTheme.addEventListener('change', (e) => {
+    console.log("toggle")
+    if (toggleTheme.checked === true) {
+        console.log('checked')
+        document.documentElement.classList.remove("light")
+        document.documentElement.classList.add("dark")
+        window.localStorage.setItem('mode', 'dark');
+      } else {
+        document.documentElement.classList.remove("dark")
+        document.documentElement.classList.add("light")
+        window.localStorage.setItem('mode', 'light');
+      }
+});
+
