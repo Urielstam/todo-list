b80da80442686a3b64afd3b2b252c7d805576fa3
Urielstam <urielstamelman@gmail.com>
2023-01-14 20:02:38 +0200

    Make dark theme the defualt theme, and make completed tasks crossed out input better design for dark theme

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import { createNew, displayAllTasks } from './create-task';
 import { openCloseSidebarModule, toggleThemeModule,  } from './theme-layout-utils';
 import { formUtilsModule } from "./display-content";
 
-// displayAllTasks();
-// formUtilsModule.editItem();
-// openCloseSidebarModule;
-// toggleThemeModule;
-// formUtilsModule;
+const todoList = document.querySelector('.todo-list');
+const allTasks = document.getElementById('all-tasks');
+const todaysTasks = document.getElementById('today-tasks')
+const mainTitle = document.querySelector('.main-title');
 
+allTasks.addEventListener('click', (e) => {
+    mainTitle.innerText = "All Tasks";
+})
+
+todaysTasks.addEventListener('click', (e) => {
+    mainTitle.innerText = "Today";
+})
