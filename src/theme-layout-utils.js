f45eb28a107db2d83fffb2686390de2ca359a7ee
Urielstam <urielstamelman@gmail.com>
2023-01-17 23:04:32 +0200

    Add maxlength to title names

--- src/theme-layout-utils.js
+++ src/theme-layout-utils.js
@@ -19,15 +19,33 @@ export const openCloseSidebarModule = (() => {
     const side = document.querySelector('nav');
     side.style.width = "250px"
     btn.addEventListener('click', (e) => {
-        changeWidth(side);
+        if(window.innerWidth <= 700) {
+            toggleSidebar()
+        } else {
+            changeWidth(side);
+        }
     });
 
     const changeWidth = (side) => {
         if(side.style.width === "250px") {
             side.style.width = "0";
+            side.style.zIndex = "1";
         } else {
             side.style.width = "250px";
+            side.style.zIndex = "10";
         }
     }
 })();
+
+const toggleSidebar = () => {
+    let grid = document.querySelector(".contents");
+    let currentWidth = window.innerWidth;
+    if (currentWidth <= 700) {
+      if (grid.style.gridTemplateAreas === "\"header header\" \"sidebar main\"") {
+        grid.style.gridTemplateAreas = "\"header\" \"sidebar\" \"main\"";
+      } else {
+        grid.style.gridTemplateAreas = "\"header header\" \"sidebar main\"";
+      }
+    }
+}
 
